bc02cbb78ea329b2329dd95d5835e02fae55083b
Ellis Percival <flyte@failcode.co.uk>
2017-08-03 11:41:08 +0100

    Add a bunch of schleem

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,40 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("Github", "https://github.com/flyte"),
+    ("Stack Overflow Careers", "https://stackoverflow.com/cv/ellis"),
+    ("Mixcloud", "https://mixcloud.com/ellisfp")
+)
+
+PROJECTS = [
+    dict(
+        name="pi-mqtt-gpio",
+        url="https://github.com/flyte/pi-mqtt-gpio",
+        description=("Expose the Raspberry Pi GPIO pins (and/or external IO mo"
+                     "dules such as the PCF8574) to an MQTT server. This allow"
+                     "s pins to be read and switched by reading or writing mes"
+                     "sages to MQTT topics.")
+    ),
+    dict(
+        name="pcf8574",
+        url="https://github.com/flyte/pcf8574",
+        description="A library for the pcf8574 I2C IO expander chip"
+    ),
+    dict(
+        name="apcaccess",
+        url="https://github.com/flyte/apcaccess",
+        description=("Pure Python clone of <a href=\"http://linux.die.net/man/"
+                     "8/apcaccess\">apcaccess</a> to allow programmatic use of"
+                     " APC UPS status data.")
+    ),
+    dict(
+        name="django-generic-counter",
+        url="https://github.com/0x07Ltd/django-generic-counter",
+        description=("A simple django app which contains a model to use as a c"
+                     "ounter for anything you'd like.")
+    )
+]
 
 DEFAULT_PAGINATION = 10
 
@@ -39,13 +71,9 @@ THEME = "themes/elegant-custom"
 LANDING_PAGE_ABOUT = dict(
     title="Beta not Best",
     details="""
-    <div>Freelance backend developer, DevOps and tech wrangler.</div>
-    <hr />
-    <ul>
-      <li><a href="https://github.com/flyte">Github</a></li>
-      <li><a href="https://stackoverflow.com/cv/ellis">Stack Overflow Careers CV</a></li>
-    </ul>
-    <hr />
+    Freelance backend developer, DevOps and tech wrangler. Fan of home automation, embedded devices,
+     sofas and hammocks.
+    <p><em><a href="https://youtu.be/_fGujzulsas">Bit</a> is my spirit animal.</em></p>
     """
 )
 
@@ -58,4 +86,5 @@ PLUGINS = ['sitemap', 'extract_toc', 'tipue_search']
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
 COMMENTS_INTRO = "What do you think? Did I get something wrong? Could this be"\
                  " done better? Let me know below."
-DIRECT_TEMPLATES = (("search",))
+DIRECT_TEMPLATES = (("search", "index"))
+SOCIAL_PROFILE_LABEL = "More of Me"
